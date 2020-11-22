7dc6cbdd52e15cba87cb30770e4460f169594098
tidiview <francois.vidit@laposte.net>
2020-11-22 19:57:24 +0100

    tryed new index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-// Import React dependencies.
-import React, { useEffect, useMemo, useState } from "react";
-// Import the Slate editor factory.
-import { createEditor } from 'slate'
-
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react'
+import React, { useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 
 const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
-  // Add the initial value when setting up our state.
+  const editor = useMemo(() => withReact(createEditor()), []);
   const [value, setValue] = useState([
     {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ])
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }]
+    }
+  ]);
 
   return (
-    <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+    <Slate 
+      editor={editor} 
+      value={value} 
+      onChange={value => setValue(value)}
+    >
       <Editable />
     </Slate>
-  )
-}
+  );
+};
+
+export default App;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
